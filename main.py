241311f1a7bb03cf94be99dab45accfb618a4056
rainvm <coyne.ryan3@gmail.com>
2023-05-01 10:22:41 -0400

    Green's Update

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class Title(Scene):
 class Limit(Scene):
     def construct(self):
         self.next_section(type=PresentationSectionType.NORMAL)
-        title = Text("Limits", color=BLACK)
+        title = Text("Limits", color = BLACK)
         title.set_y(3.5)
         title.set_x(-4)
         axes = Axes(
@@ -40,29 +40,43 @@ class Limit(Scene):
             y_range=[0, 3, 0.5],
             x_length=4,
             y_length=4,
-            axis_config={'font_size': 18}
+            axis_config={'font_size': 18, "stroke_color": BLACK}
         )
         axes.set_y(-1)
-        axes.axes.color = BLACK
         axes.add_coordinates({0, 0.5, 1, 1.5, 2}, {0, 0.5, 1, 1.5, 2, 2.5, 3})
         axes.x_axis.numbers.color = BLACK
         axes.y_axis.numbers.color = BLACK
         limf = axes.plot(
             lambda t: t + 1,
-            color=RED,
+            color=BLACK,
             x_range=[0, 2]
+        )
+        limfRight = axes.plot(
+            lambda t: t + 1,
+            color=BLACK,
+            x_range=[1,0],
+        )
+        limfLeft = axes.plot(
+            lambda t: t + 1,
+            color=BLACK,
+            x_range=[0, 1]
         )
         limitFunctionText = MathTex(r"f(x)=\frac{x^2-1}{x-1}", color=BLACK, font_size=24)
         limitFunctionText.set_x(-3.5)
         limitFunctionText.set_y(2.5)
-        vert = axes.plot_implicit_curve(lambda x, y: x - 1, color=BLACK)
-        vert.y_range = {0, 2}
         hole = Circle(0.0001, fill_opacity=1, fill_color=WHITE,)
         holePos = axes.coords_to_point(1, 2)
         hole.set_x(holePos[0])
         hole.set_y(holePos[1])
-        self.add(axes, limitFunctionText, title)
+        rightLimitDot = Dot().set_color(RED_E)
+        leftLimitDot = Dot().set_color(RED_E)
+        self.add(limitFunctionText, title)
+        self.play(Create(axes))
         self.play(Create(limf))
         self.next_section(type=PresentationSectionType.NORMAL)
         self.add(hole)
         self.play(hole.animate.scale(500))
+        self.next_section(type=PresentationSectionType.NORMAL)
+        self.play(Create(limfRight), Create(limfLeft), Create(rightLimitDot), Create(leftLimitDot))
+        self.wait()
+        self.play(MoveAlongPath(rightLimitDot, limfRight), rate_func=linear)
